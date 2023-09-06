adaf40287f599ee2a62c28269fcca601590df9e5
crushedbyamallet <rm550242@fiap.com.br>
2023-09-06 10:32:48 -0300

    medeusdj

--- aula-rotas/src/routes/Produtos/index.jsx
+++ aula-rotas/src/routes/Produtos/index.jsx
@@ -1,19 +1,27 @@
-import { Link } from "react-router-dom"
-import { listaProdutos } from "../components/listaProdutos"
+import { listaProdutos } from '../../src/components/produtos';
+import { Link } from 'react-router-dom';
 
+export default function Produtos() {
 
-export default function Produto(){
-    
-    return(
-        <main>
-            <h1>Produtos</h1>
-            {listaProdutos.map(prod = > (
-                <div key={prod.id}>
-                    <Link to={'/produtos/editar/${prod.id}'}>
-                        Editar o produto: {prod.nome}
-                    </Link>
-                </div>
-            ))}
-        </main>
-    )
-} 
+  return (
+
+    <main>
+
+      <h1>Produtos</h1>
+
+      {listaProdutos.map((prod) => (
+
+        <div key={prod.id}>
+          <Link to={`/produtos/editar/${prod.id}`}>
+            Editar o produto: {prod.nome}
+          </Link>
+
+        </div>
+
+      ))}
+
+    </main>
+
+  );
+
+}
